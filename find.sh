aec4e766200521fd16b5bc25052764349f245b15
Palanimks <palanivelmks@gmail.com>
2019-06-06 05:47:12 +0000

    given access for year

--- find.sh
+++ find.sh
@@ -6,7 +6,7 @@ APIURL="http://www.omdbapi.com/?apikey=9fc0e77e&t=$movie"
 #echo $APIURL
 data=`curl -s $APIURL`
 name=`echo $data | jq .Title`
-#year=`echo $data | jq .Year`
+year=`echo $data | jq .Year`
 ratings=`echo $data | jq .Ratings[1].Value`
 echo "Movie Name = $name"
 #echo "Year = $year"
